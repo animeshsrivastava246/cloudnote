12b1e1fa9045703f0ca670b4b1816f8864efad39
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2021-09-07 14:25:23 +0530

    Added Express

---
+++ backend/db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false";
+const connectToMongo = ()=>{
+    mongoose.connect(mongoURI,()=>{
+        console.log("Connected to Mongo");
+    })
+}
+module.exports = connectToMongo;
