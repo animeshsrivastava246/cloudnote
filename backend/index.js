01b5a05fc914c23d4507f110dc4ec42768395a05
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2021-09-16 23:09:04 +0530

    Temporary

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,12 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello Anni!')
 })
+app.get('/xy/login', (req, res) => {
+  res.send('Hello Login!')
+})
+app.get('/xy/signup', (req, res) => {
+  res.send('Hello Signup!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
