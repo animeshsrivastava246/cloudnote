e5b214549c9cd3561f6cc4da49c474fd994e6945
ANIMESH SRIVASTAVA <animeshsrivastava246246@gmail.com>
2021-10-06 19:33:18 +0530

    56 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is my cloudNote</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Switch>
+            <Route exact path="/"> <Home/> </Route>
+            <Route exact path="/about"> <About/> </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+            This is Home
+        </div>
+    )
+}
+
+export default Home;
